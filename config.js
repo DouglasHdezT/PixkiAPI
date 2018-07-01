3efae85b7fc174b8c5ba4d3672e1fdcf52d2fdfc
Douglas_HdezT <00004516@uca.edu.sv>
2018-07-01 13:38:35 -0600

    Base de datos en linea

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports={
     port: process.env.PORT || 3001,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/PixkiAPI',
+    db: process.env.MONGODB || 'mongodb+srv://admin_pixki:pizza2018!@cluster0-vyjof.mongodb.net/test?retryWrites=true',
     SECRET:'pvt031q3l0l34'
 }
