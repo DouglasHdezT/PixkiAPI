f8526ac2b0fbb795689db4df8bdb1584c2d71468
CarlosDubon <00058016@uca.edu.sv>
2018-06-30 18:46:11 -0600

    finalizacion de modelo sintoma

--- models/symptomModel.js
+++ models/symptomModel.js
@@ -1,3 +1,20 @@
 'use strict'
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const symptonSchema = Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    description:{
+        type:String,
+        require:true
+    },
+    date_time:{
+        type:Date,
+        require:Date.now
+    }
+});
+
+module.exports = mongoose.model("Symptom",symptonSchema);
