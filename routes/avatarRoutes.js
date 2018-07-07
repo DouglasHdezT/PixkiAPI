09a482ec4d49dbcdb5ba2d357333b7826fa1f9e4
CarlosDubon <00058016@uca.edu.sv>
2018-07-07 16:22:37 -0600

    avatar

--- routes/avatarRoutes.js
+++ routes/avatarRoutes.js
@@ -4,7 +4,7 @@ const express = require('express');
 const avatarCtrl = require('../controllers/avatarController');
 const api = express.Router();
 
-api.post('/avatar',avatarCtrl.insertBanner);
-api.get('/avatar',avatarCtrl.getBanners);
+api.post('/avatar',avatarCtrl.insertAvatar);
+api.get('/avatar',avatarCtrl.getAvatars);
 
 module.exports = api;
